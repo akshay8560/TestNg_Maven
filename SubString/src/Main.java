6e43f7c1a549e74fba8c5fe55f7cad2b30f28af8
Akshay Kumar <doodwal.akshay@tftus.com>
2023-08-23 12:52:24 +0530

    new commit

--- SubString/src/Main.java
+++ SubString/src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
       String s1="AkshayKumar";
         System.out.println( s1);
-        System.out.println(s1.toString());
+        //System.out.println(s1.toString());
+
+
 
         System.out.println(s1.substring(6));
+        System.out.println(s1.substring(3));
         System.out.println(s1.substring(0,6));
     }
 
