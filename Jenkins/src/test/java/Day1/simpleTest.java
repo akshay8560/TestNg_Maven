1a3e96397caab3461c0cec96119d5513c838b3a0
Akshay Kumar <doodwal.akshay@tftus.com>
2023-10-04 10:57:18 +0530

    Commit

---
+++ Jenkins/src/test/java/Day1/simpleTest.java
@@ -0,0 +1,13 @@
+package Day1;
+
+import org.testng.annotations.Test;
+public class simpleTest {
+    @Test
+    void verifyTest(){
+        System.out.println("login successfully");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Seleniumproject/src/sampleapp/testclass.java
@@ -0,0 +1,10 @@
+package sampleapp;
+
+
+
+public class testclass {
+    public static void main(String[] args) {
+        System.out.println("Hello Akshay Kumar");
+        System.out.println("Heyeeeeeeee Akshay Kumar");
+    }
+}
